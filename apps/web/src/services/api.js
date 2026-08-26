fdf7dc511030887b7adc646ec5ef2611b115b9e4
akshaybraj71-blip <akshaybraj71@gmail.com>
2026-08-26 11:58:15 +0530

    fix: remove insecure token storage from localStorage, use httpOnly cookies via withCredentials

--- apps/web/src/services/api.js
+++ apps/web/src/services/api.js
@@ -2,17 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("crm_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default api;
