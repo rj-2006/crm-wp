dc890765491fe286141ff743809328a06600d047
Saksham verma <sakshamv855@gmail.com>
2026-08-27 22:55:56 +0530

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: process.env.CORS_ORIGIN || true,
     credentials: true,
   });
   
